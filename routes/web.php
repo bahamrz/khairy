7b90a672b7557ef1a11e1ba34ccb09bcde249767
bahamrz <baha.mrz@gmail.com>
2019-12-10 00:10:14 +0200

    Added Routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+route::get('/AboutUs', function(){
+    return view('AboutUs');
+});
+route::get('/ContactUs', function(){
+return view('ContactUs');
+});
+route::get('/SignIn',function(){
+    return view('SignIn');
+});
+route::get('/SignUp',function(){
+    return view('SignUp');
+});
+route::get('Events',function(){
+    return view('Events');
+});
+route::get('Donations',function(){
+    return view('Donations');
+});
+route::get('Gallery',function(){
+    return view('Gallery');
 });
