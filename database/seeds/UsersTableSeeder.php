2bb3384d5ba8f1e41c2c5b894b2698578616a8c6
Ahmed sbeta <xahmed.sbx@gmail.com>
2019-12-28 19:36:31 +0200

    fixed donation reservation relation

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,24 +14,24 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
+        // User::truncate();
         DB::table('role_user')->truncate();
 
         $adminRole = Role::where('name', 'admin')->first();
         $orgadminRole = Role::where('name', 'orgadmin')->first();
         $userRole = Role::where('name', 'user')->first();
-            
+
         $admin = User::create([
             'name' => 'Admin User',
             'email' => 'admin@admin.com',
             'password' => Hash::make('12345678')
         ]);
-        
+
         $orgadmin = User::create([
             'name' => 'Org Admin User',
             'email' => 'orgadmin@orgadmin.com',
             'password' => Hash::make('12345678')
-        ]);        
+        ]);
 
         $user = User::create([
             'name' => 'User',
